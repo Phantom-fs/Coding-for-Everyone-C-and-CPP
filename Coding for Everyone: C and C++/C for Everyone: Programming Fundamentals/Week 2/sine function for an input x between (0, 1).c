24a6dfc603986a970cc697deeb4b19d1afa552b2
Phantom-fs <101197900+Phantom-fs@users.noreply.github.com>
2022-04-04 00:34:02 +0530

    Create sine function for an input x between (0, 1).c

---
+++ Coding for Everyone: C and C++/C for Everyone: Programming Fundamentals/Week 2/sine function for an input x between (0, 1).c
@@ -0,0 +1,31 @@
+/*
+sine function for an input x between (0, 1) (week 2)
+*/
+//Coder: Phantom-fs
+#include <stdio.h>
+#include <math.h>   //maths library have functions like sqrt, sin, cos etc.
+
+int main()
+{
+  printf("Program to calculate sine function for an input x between (0, 1) in radians\n\n");    //informing what the program is about
+
+    double value = 0.0, result = 0.0;      //declaring in double format
+
+    printf("Enter the number b/w (0,1) (eg. 0.542) : ");
+    scanf("%lf",&value);
+
+  //loop statement for condition that value entered must be between (0,1) ****not inclusive*****, for inclusive we can use <= & >=
+  if ((value > 0)&&(value < 1)) {
+
+    result = sin(value);      //Calculate the Sine of value
+
+    printf("\nThe Sine of %lf is %lf\n", value, result);      //Display the result of the calculation
+  }
+
+  //else the value was entered incorrect
+  else {
+     printf("Value %lf was not between (0,1), please try again!\n", value);
+  }
+
+ return 0;
+}
