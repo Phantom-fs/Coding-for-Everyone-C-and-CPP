86562602b89b25f23c730ab4375894f35ff950d0
Phantom-fs <101197900+Phantom-fs@users.noreply.github.com>
2022-04-04 00:17:48 +0530

    Fix Dr. P's mistake (week-1)
    Project about printing a poem using printf command in C from stdio.h library

--- Coding for Everyone: C and C++/C for Everyone: Programming Fundamentals/Week 1/Print a Poem.c
+++ Coding for Everyone: C and C++/C for Everyone: Programming Fundamentals/Week 1/Print a Poem.c
@@ -1,5 +1,5 @@
-/*Print Poem
-  7 march 22
+/*
+Program about printing a poem
 */
 //Coder: Phantom-fs
 #include <stdio.h>
@@ -8,7 +8,7 @@ int main (void)
 {
   //print title
   //In a Station of the Metro by EZRA POUND
-  printf("In a station of the Metro\nby EZRA POUND\n");
+  printf("In a station of the Metro\nby EZRA POUND\n"); //here \n is used for printing or changing to new line
 
   //print poem
   printf("\nThe apparition of these faces in the crowd:\nPetals on a wet, black bough.");
