f1defa50f925db70c120e0f716a8b01e2d98ad7f
Phantom-fs <101197900+Phantom-fs@users.noreply.github.com>
2022-04-04 00:14:47 +0530

    Add files via upload

---
+++ Coding for Everyone: C and C++/C for Everyone: Programming Fundamentals/Week 1/Print a Poem.c
@@ -0,0 +1,18 @@
+/*Print Poem
+  7 march 22
+*/
+//Coder: Phantom-fs
+#include <stdio.h>
+
+int main (void)
+{
+  //print title
+  //In a Station of the Metro by EZRA POUND
+  printf("In a station of the Metro\nby EZRA POUND\n");
+
+  //print poem
+  printf("\nThe apparition of these faces in the crowd:\nPetals on a wet, black bough.");
+
+  return 0;
+
+}
